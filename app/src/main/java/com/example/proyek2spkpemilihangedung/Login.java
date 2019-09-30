5c72f428c96a70d56df460f54a317b331afc0824
Wijip <wiji.prabowo0857@gmail.com>
2019-09-30 10:17:46 +0700

    first commit

---
+++ app/src/main/java/com/example/proyek2spkpemilihangedung/Login.java
@@ -0,0 +1,27 @@
+package com.example.proyek2spkpemilihangedung;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void postLogin (View View) {
+        Intent intent = new Intent (this, SuccessLogin.class);
+        startActivity(intent);
+    }
+
+    public void onCreate1 (View View) {
+        Intent intent = new Intent (this, CreateAccount.class);
+        startActivity(intent);
+    }
+
+}
