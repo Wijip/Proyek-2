4fcd8858e5e73eff885a966531035eb6d2c3c379
Wijip <wiji.prabowo0857@gmail.com>
2019-10-01 12:48:09 +0700

    Membuat halaman Success Create Acount

--- app/src/main/java/com/example/proyek2spkpemilihangedung/CreateAccount.java
+++ app/src/main/java/com/example/proyek2spkpemilihangedung/CreateAccount.java
@@ -2,7 +2,9 @@ package com.example.proyek2spkpemilihangedung;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class CreateAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
     }
+    public void SuccessCreate(View View){
+        Intent intent = new Intent(this, SuccessCreateAccount.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/proyek2spkpemilihangedung/SuccessCreateAccount.java
+++ app/src/main/java/com/example/proyek2spkpemilihangedung/SuccessCreateAccount.java
@@ -2,7 +2,9 @@ package com.example.proyek2spkpemilihangedung;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SuccessCreateAccount extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class SuccessCreateAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success_create_account);
+    }
+    public void Home(View View){
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
     }
 }
